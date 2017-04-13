2c5b84054577be177e60ddac4ed85a7947ccd88d
Erin R Martin <erinrmartin@yahoo.com>
2017-04-13 12:10:33 -0700

    Events Calendar
    Assignment 2

---
+++ EventsCal2/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Events Calendar</title>
+   
+    <link href="StyleSheet.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h3>Events Calendar April 2017</h3>
+        <asp:Label ID="EventLabel" runat="server" Text=""></asp:Label>
+        <asp:Calendar ID="EventCalendar" runat="server" OnSelectionChanged="EventCalendar_SelectionChanged1"></asp:Calendar>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ EventsCal2/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+     protected void EventCalendar_SelectionChanged1(object sender, EventArgs e)
+    {
+        {
+            EventLabel.Text = "";
+            string[,] events = new string[6, 2];
+
+            events[0, 0] = "4/13/2017";
+            events[0, 1] = "Classes at SCC";
+            events[1, 0] = " 4/14/2017";
+            events[1, 1] = "Work but it's Friday!";
+            events[2, 0] = "4/17/2017";
+            events[2, 1] = "Work meh oh well";
+            events[3, 0] = "4/20/2017";
+            events[3, 1] = "Yeah buddy! Meeting on the Green";
+            events[4, 0] = "4/21/2017";
+            events[4, 1] = "Payday!!!!";
+            events[5, 0] = "4/30/2017";
+            events[5, 1] = "Submit timesheet";
+
+            string dates = EventCalendar.SelectedDate.ToShortDateString();
+
+
+            for (int i = 0; i < 6; i++) //for loop
+            {
+                if (dates.Equals(events[i, 0]))
+                {
+                    EventLabel.Text += events[i, 1] + "<br/>";
+                }
+            }
+
+            if (EventLabel.Text == "") EventLabel.Text = dates;
+
+
+        }
+    }
+}
