2c73e5888cc2098b7d3e6b106a4653ba7b34f3e4
Erin R Martin <erinrmartin@yahoo.com>
2017-05-11 12:00:05 -0700

    Add files via upload

---
+++ Homework5/ICommAsstReg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICommAsstReg" in both code and config file together.
+[ServiceContract]
+public interface ICommAsstReg
+{
+    [OperationContract]
+    int Login(string user, string password);
+
+    [OperationContract]
+    bool RegisterPerson(PersonInfo pi);
+
+    [OperationContract]
+    List<GrantRequest> GetGrantsbyRequest();
+
+    [OperationContract]
+    List<GrantReview> GetGrantsByReview();
+
+   
+    
+}
+
+[DataContract]
+public class PersonInfo
+{
+    [DataMember]
+    public string lastname { get; set; }
+
+    [DataMember]
+    public string firstname { get; set; }
+
+    [DataMember]
+    public string email { get; set; }
+
+    [DataMember]
+    public string password { get; set; }
+
+    [DataMember]
+    public string ApartmentNumber { get; set; }
+
+    [DataMember]
+    public string Street { get; set; }
+
+    [DataMember]
+    public string City { get; set; }
+
+    [DataMember]
+    public string Zipcode { get; set; }
+
+    [DataMember]
+    public string HomePhone { get; set; }
+
+    [DataMember]
+    public string WorkPhone { get; set; }
+
+}
